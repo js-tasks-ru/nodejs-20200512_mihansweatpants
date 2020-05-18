90d83f042ea745a606a8b2e7cf7dfec5174d41d5
mihansweatpants <mihansweatpants@gmail.com>
2020-05-18 20:14:20 +0300

    Add solution for 0-module 1-task

--- 0-module/1-task/sum.js
+++ 0-module/1-task/sum.js
@@ -1,5 +1,9 @@
 function sum(a, b) {
-  /* ваш код */
+  if (typeof a !== 'number' && typeof b !== 'number') {
+    throw new TypeError('Arguments should be of type number');
+  }
+
+  return a + b;
 }
 
 module.exports = sum;
